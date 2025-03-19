764edcb8879c4c44514e60bd10bbdbab173437ea
AhsanHassan5 <minepersonalmailsn@gmail.com>
2025-03-20 00:10:34 +0500

    hehe

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
           page: () => const RecipePage(),
         )
       ],
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: initialPage,
     );
   }
